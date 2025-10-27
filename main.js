5f4a2661f87c17b20c7dd75a30adc918984e94d3
GameStuff <66188005+game-stuff-official@users.noreply.github.com>
2025-10-26 19:02:50 -0500

    Added the ability to add your own files

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // let playbackTime = 10000
 let myAudioElement = document.getElementById('bad-to-the-bone');
 let myAudioElementId;
+let customAudioElementId;
+
+let customFile;
+let customPlayer = document.getElementById("customPlayer");
 
 function playSound(){
     let breakTime = document.getElementById("bad-to-the-bone-break-time").value * 1000;
@@ -19,4 +23,29 @@ function playSound(){
 function stopSound(){
     document.getElementById("bad-to-the-bone-container").classList.remove("active");
     clearTimeout(myAudioElementId);
+}
+
+function updateCustomPlayer() {
+    customFile = document.getElementById("customFile").files[0];
+    customPlayer = document.getElementById("customPlayer");
+    customPlayer.src = URL.createObjectURL(customFile);
+}
+
+function playCustomSound(){
+    let breakTime = document.getElementById("custom-break-time").value * 1000;
+    breakTime *= Math.random();
+    document.getElementById("custom-container").classList.add("active");
+    customAudioElementId = setTimeout(function(){
+        customPlayer.play();
+        playCustomSound();
+    }, breakTime);
+
+    // myAudioElement.play();
+    // myAudioElement.pause();
+    // myAudioElement.volume = 0.5;
+}
+
+function stopCustomSound(){
+    document.getElementById("custom-container").classList.remove("active");
+    clearTimeout(customAudioElementId);
 }
