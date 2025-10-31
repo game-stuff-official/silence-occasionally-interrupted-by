b2e9837dff81b21f478f21890f6844dd09062f9d
GameStuff <66188005+game-stuff-official@users.noreply.github.com>
2025-10-30 21:05:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 class Sound{
-    constructor(elementSuffix){
+    constructor(elementSuffix, isCustom, fileLocation){
+        // this.fileLocation = this.fileLocation;
         this.elementSuffix = elementSuffix;
         this.timeoutId = 0;
         // this.randomized = document.getElementById(elementSuffix + "-random").checked;
         // this.breakTime = document.getElementById(elementSuffix + "-break-time").value * 1000;
         this.player = document.getElementById(elementSuffix + "-player");
         this.container = document.getElementById(elementSuffix + "-container");
+        this.isCustom = isCustom;
+        let guh;
+        if(isCustom){
+            this.fileUpload = document.getElementById(elementSuffix + "-file-upload");
+            this.fileUpload.addEventListener("change", () => this.updateCustomSound()); 
+            guh = `<p>
+                        <input type="file" id="${elementSuffix}-file-upload" accept="audio/*">
+                    </p>`
+        }
+        this.elementTemplate = `
+<section id="${this.elementSuffix}-container" class="sound">
+    <h2>${this.elementSuffix}</h2>
+    <audio id="${this.elementSuffix}-player">
+        <source src="${this.fileLocation}.mp3">
+    </audio>
+    ${guh}
+    <p>
+        <button id="${this.elementSuffix}-play">Play Sound</button>
+        <button id="${this.elementSuffix}-stop">Stop Sound</button>
+    </p>
+    <p>
+    Maximum time between breaks (seconds):
+        <input type="number" id="${this.elementSuffix}-break-time" min="0" max="3600">
+    </p>
+    <p>
+        Random Playback
+        <input type="checkbox" id="${this.elementSuffix}-random">
+    </p>
+</section>`
+
+        document.getElementById(this.elementSuffix + "-play").addEventListener("click", () => this.playSound());
+        document.getElementById(this.elementSuffix + "-stop").addEventListener("click", () => this.stopSound());
     }
     get randomized(){
         return document.getElementById(this.elementSuffix + "-random").checked;
@@ -17,59 +50,34 @@ class Sound{
             return document.getElementById(this.elementSuffix + "-break-time").value * 1000;
         }
     }
-    play(){
-        this.player.play();
-    }
     playSound(){
         console.log("Attempting to play sound.");
         this.container.classList.add("active");
+        
+        this.timeoutId = setTimeout(() => {
+            console.log(this);
+            this.player.currentTime = 0;
+            this.player.play();
 
-        let THAT = this;
-        this.timeoutId = setTimeout(function(that = THAT){
-            console.log(that);
-            that.player.play();
-            // that.stopSound();
-            that.playSound();
-        }, THAT.breakTime)
+            this.playSound()
+        }, this.breakTime)
     }
     stopSound(){
         document.getElementById(this.elementSuffix + "-container").classList.remove("active");
         clearTimeout(this.timeoutId);
+        this.player.pause();
+    }
+    updateCustomSound(){
+        if(this.isCustom){
+            let file = this.fileUpload.files[0];
+            this.player.src = URL.createObjectURL(file);
+        }
     }
 }
-let pipe = new Sound("metal-pipe");
+let custom1 = new Sound("custom1", true);
+
+let pipe = new Sound("metal-pipe", false);
 // pipe.playSound();
 
-let badToTheBone = new Sound("bad-to-the-bone");
-
-
-let customAudioElementId;
-let customAudioRandomized = false;
-
-let customFile;
-let customPlayer = document.getElementById("customPlayer");
-
-function updateCustomPlayer() {
-    customFile = document.getElementById("customFile").files[0];
-    customPlayer = document.getElementById("customPlayer");
-    customPlayer.src = URL.createObjectURL(customFile);
-}
-
-function playCustomSound(){
-    customAudioRandomized = document.getElementById("custom-random").checked;
-    let breakTime = document.getElementById("custom-break-time").value * 1000;
-    if(customAudioRandomized){
-        breakTime *= Math.random();
-    }
-    document.getElementById("custom-container").classList.add("active");
-    customAudioElementId = setTimeout(function(){
-        customPlayer.play();
-        stopCustomSound();
-        playCustomSound();
-    }, breakTime);
-}
-
-function stopCustomSound(){
-    document.getElementById("custom-container").classList.remove("active");
-    clearTimeout(customAudioElementId);
-}
+let badToTheBone = new Sound("bad-to-the-bone", false);
+// badToTheBone.bindButtons();
